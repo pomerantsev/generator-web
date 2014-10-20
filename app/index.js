2a2038ab07595b844295d815ac045d3291bf3080
Pavel Pomerantsev <pomerantsevp@gmail.com>
2014-10-20 11:49:31 +0400

    Prompt name

--- app/index.js
+++ app/index.js
@@ -6,5 +6,17 @@ module.exports = yeoman.generators.Base.extend({
   },
   method2: function () {
     console.log('method 2');
+  },
+  promptName: function () {
+    var done = this.async();
+    this.prompt({
+      type: 'input',
+      name: 'name',
+      message: 'Your project name',
+      default: this.appname
+    }, function (answers) {
+      this.log(answers.name);
+      done();
+    }.bind(this));
   }
 });
