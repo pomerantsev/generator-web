65f2f47b0dcfd758177d501202a25554555a9ab5
Pavel Pomerantsev <pomerantsevp@gmail.com>
2014-10-25 12:53:21 +0400

    Add a template for package.json

--- app/index.js
+++ app/index.js
@@ -1,22 +1,24 @@
 var yeoman = require('yeoman-generator');
 
 module.exports = yeoman.generators.Base.extend({
-  method1: function () {
-    console.log('method 1');
+  prompting: {
+    promptName: function () {
+      var done = this.async();
+      this.prompt({
+        type: 'input',
+        name: 'projectName',
+        message: 'Your project name'
+      }, function (answers) {
+        this.projectName = answers.projectName;
+        done();
+      }.bind(this));
+    }
   },
-  method2: function () {
-    console.log('method 2');
-  },
-  promptName: function () {
-    var done = this.async();
-    this.prompt({
-      type: 'input',
-      name: 'name',
-      message: 'Your project name',
-      default: this.appname
-    }, function (answers) {
-      this.log(answers.name);
-      done();
-    }.bind(this));
+  configuring: {
+    copyFiles: function () {
+      this.template('_package.json', 'package.json', {
+        projectName: this.projectName
+      });
+    }
   }
 });
